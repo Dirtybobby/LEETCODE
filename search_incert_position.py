8a316e7ba8423fa8dc7645bd07b41bc27fe26d5c
Bogdan Kravchenko <bkblogpost@gmail.com>
2024-02-18 12:36:17 +0200

    Search incert position, not best decision

--- search_incert_position.py
+++ search_incert_position.py
@@ -1,6 +1,16 @@
-def main():
-    pass
-    
-    
-if __name__ == '__main__':
-    main()
+from typing import List
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for num in nums:
+            if target == num:
+                return nums.index(num)
+            else:
+                nums.append(target)
+                nums.sort()
+                return nums.index(target)
+
+solution = Solution()
+
+nums = [1, 3, 6]
+target = 5
+print(solution.searchInsert(nums, target))
